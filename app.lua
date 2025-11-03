46f06dce59b2abf1540d15766b3bf6212d8c69a2
Rodney Gauna <rodneygauna@gmail.com>
2025-11-03 15:18:14 -0800

    feat: Enable etlua in app and add .keep files to controllers, models, and views

--- app.lua
+++ app.lua
@@ -1,5 +1,8 @@
+-- app.lua
 local lapis = require("lapis")
+
 local app = lapis.Application()
+app:enable("etlua")
 
 app:get("/", function()
   return "Welcome to Lapis " .. require("lapis.version")
