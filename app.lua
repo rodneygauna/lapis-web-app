8d734c289003d47f68978a132971624d4a306333
Rodney Gauna <rodneygauna@gmail.com>
2025-11-03 15:31:45 -0800

    feat: Update routing to use match and set layout for rendering

--- app.lua
+++ app.lua
@@ -3,9 +3,10 @@ local lapis = require("lapis")
 
 local app = lapis.Application()
 app:enable("etlua")
+app.layout = require("views.layout")
 
-app:get("/", function()
-  return "Welcome to Lapis " .. require("lapis.version")
+app:match("/", function(self)
+  return { render = "index" }
 end)
 
 return app
